1c469ce6c9a1e5cf0128878375e16c9cfd264088
tech-ninjacoder <ninjacoder.anywhere@gmail.com>
2025-04-15 23:36:24 +0800

    fix: unable to assign gatepass to user

---
+++ database/migrations/2025_04_15_190314_set_default_current_timestamp_start_date_gate_pass_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("
+            ALTER TABLE gate_pass_user
+            MODIFY start_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+       DB::statement("
+            ALTER TABLE gate_pass_user
+            MODIFY start_date TIMESTAMP NULL DEFAULT NULL
+        ");
+    }
+};
